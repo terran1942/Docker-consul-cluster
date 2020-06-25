4b5ee9018bc1d6ef9d7fd39073cc14569a480b12
yongliang li <terran1942@gmail.com>
2020-06-25 12:28:14 +0800

    Add shell

---
+++ consul-a.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d --name consul-server --network=host -v /root/consul/data:/consul/data -v /root/consul/config:/consul/config consul:1.7 agent -server -bootstrap-expect=3 -ui -bind=<your server A IP> -client=0.0.0.0
---
+++ consul-client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d --name consul-client --network=host -v /root/consul/data:/consul/data -v /root/consul/config:/consul/config consul:1.7 agent -bind=<your client IP> -retry-join=<your server A IP>
